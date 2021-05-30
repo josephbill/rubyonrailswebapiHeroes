720dfd21c2e5662b51c159afd7bf5aaa8b6f408e
ihollander <ianholla@gmail.com>
2021-05-30 11:36:13 -0400

    Fixed routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Hero from "./Hero";
 import Home from "./Home";
 import HeroPowerForm from "./HeroPowerForm";
+import Power from "./Power";
+import PowerEditForm from "./PowerEditForm";
 
 function App() {
   return (
@@ -13,7 +15,12 @@ function App() {
           <Route exact path="/hero_powers/new">
             <HeroPowerForm />
           </Route>
-          <Route exact path="/powers/:id"></Route>
+          <Route exact path="/powers/:id/edit">
+            <PowerEditForm />
+          </Route>
+          <Route exact path="/powers/:id">
+            <Power />
+          </Route>
           <Route exact path="/heroes/:id">
             <Hero />
           </Route>
